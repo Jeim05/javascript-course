c14b90665a00692144cc6bef3a698daa7ab53b18
Jeim05 <lopezvegajeimmy@gmail.com>
2024-05-28 22:27:25 -0600

    Mostrar los productos en el carrito

--- Proyectos/Tienda/src/carrito.js
+++ Proyectos/Tienda/src/carrito.js
@@ -1,14 +1,61 @@
 // Accedemos a los botones que me permiten abrir el carrito
-const botonesAbrirCarrito = document.querySelectorAll('[data-accion="abrir-carrito"]');
-const botonesCerrarCarrito = document.querySelectorAll('[data-accion="cerrar-carrito"]');
+const botonesAbrirCarrito = document.querySelectorAll(
+  '[data-accion="abrir-carrito"]'
+);
+const botonesCerrarCarrito = document.querySelectorAll(
+  '[data-accion="cerrar-carrito"]'
+);
 const ventanaCarrito = document.getElementById("carrito");
-const btnAgregarCarrito = document.getElementById('agregar-al-carrito');
-const producto = document.getElementById('producto');
+const btnAgregarCarrito = document.getElementById("agregar-al-carrito");
+const producto = document.getElementById("producto");
 const carrito = [];
 
 const renderCarrito = () => {
   // PARA ABRIR LA VENTA
   ventanaCarrito.classList.add("carrito--active");
+
+  carrito.forEach((productoCarrito) => {
+    // Creamos una plantilla del codigo HTML
+    const plantillaProducto = `
+  <div class="carrito__producto-info">
+    <img src="./img/tennis/1.jpg" alt="" class="carrito__thumb" />
+    <div>
+      <p class="carrito__producto-nombre">
+        <span class="carrito__producto-cantidad">${productoCarrito.cantidad} x </span>${productoCarrito.nombre}
+      </p>
+      <p class="carrito__producto-propiedades">
+        Tamaño:<span>${productoCarrito.tamano}</span> Color:<span>${productoCarrito.color}</span>
+      </p>
+    </div>
+  </div>
+  <div class="carrito__producto-contenedor-precio">
+    <button class="carrito__btn-eliminar-item" data-accion="eliminar-item-carrito">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"
+        />
+      </svg>
+    </button>
+    <p class="carrito__producto-precio">$500.00</p>
+  </div>
+    `;
+
+    const itemCarrito = document.createElement("div");
+
+    // Le agregamos a la clase de carrito__producto
+    itemCarrito.classList.add("carrito__producto");
+
+    itemCarrito.innerHTML = plantillaProducto;
+
+    // Agregamos el producto a la ventana del carrito
+    ventanaCarrito.querySelector('.carrito__body').appendChild(itemCarrito);
+  });
 };
 
 // Abrir carrito
@@ -26,12 +73,20 @@ botonesCerrarCarrito.forEach((boton) => {
 });
 
 // Agregar productos al carrito
-btnAgregarCarrito.addEventListener('click', (e) => {
+btnAgregarCarrito.addEventListener("click", (e) => {
   const id = producto.dataset.productoId;
-  const nombre = producto.querySelector('.producto__nombre').innerText;
-  const cantidad = parseInt(producto.querySelector('#cantidad').value);
-  const color = producto.querySelector('#propiedad-color input:checked').value;
-  const tamano = producto.querySelector('#propiedad-tamaño input:checked').value;
+  const nombre = producto.querySelector(".producto__nombre").innerText;
+  const cantidad = parseInt(producto.querySelector("#cantidad").value);
+  const color = producto.querySelector("#propiedad-color input:checked").value;
+  const tamano = producto.querySelector(
+    "#propiedad-tamaño input:checked"
+  ).value;
 
-  
-})
+  carrito.push({
+    id: id,
+    nombre: nombre,
+    cantidad: cantidad,
+    color: color,
+    tamano: tamano,
+  });
+});
