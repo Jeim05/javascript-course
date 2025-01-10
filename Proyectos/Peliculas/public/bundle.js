c34f9aba53b9c11af885ad4c7b20399347eabea9
Jeim05 <lopezvegajeimmy@gmail.com>
2025-01-09 21:18:57 -0600

    Mostrando datos

--- Proyectos/Peliculas/public/bundle.js
+++ Proyectos/Peliculas/public/bundle.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const cargarTitulos = (results) => {
+    const contenedor = document.querySelector('#populares .main__grid');
+    results.forEach(result => {
+        const plantilla = `
+     <div class="main__media">
+		<a href="#" class="main__media-thumb">
+			<img class="main__media-img" src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="" />
+		</a>
+		<p class="main__media-titulo">${result.title}</p>
+		<p class="main__media-fecha">2021</p>
+	</div>
+    `;
+
+    contenedor.insertAdjacentHTML('beforeend',plantilla);
+    });
+};
+
 const fetchPopulares = async() =>{
     const url = 'https://api.themoviedb.org/3/movie/popular?api_key=47643206aa052da5a1a02e515825f7de&language=es-CR&page=1';
     
@@ -16,7 +33,8 @@ const fetchPopulares = async() =>{
 };
 
 const cargar = async () =>{
-   await fetchPopulares();
+   const results = await fetchPopulares();
+   cargarTitulos(results);
 };
 
 cargar();
--- Proyectos/Peliculas/src/cargarTitulos.js
+++ Proyectos/Peliculas/src/cargarTitulos.js
@@ -1,5 +1,18 @@
 const cargarTitulos = (results) => {
-    
+    const contenedor = document.querySelector('#populares .main__grid')
+    results.forEach(result => {
+        const plantilla = `
+     <div class="main__media">
+		<a href="#" class="main__media-thumb">
+			<img class="main__media-img" src="https://image.tmdb.org/t/p/w500/${result.poster_path}" alt="" />
+		</a>
+		<p class="main__media-titulo">${result.title}</p>
+		<p class="main__media-fecha">2021</p>
+	</div>
+    `
+
+    contenedor.insertAdjacentHTML('beforeend',plantilla)
+    });
 }
 
 export default cargarTitulos;
